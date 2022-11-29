839b2e3172fe17596ae825ac52d844a191dd43fb
Yuri Kim <yuri7kim@gmail.com>
2022-11-29 16:29:34 -0500

    start of admin react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Login from './Login.js';
+import AdminList from './AdminList.js';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <AdminList />
     </div>
   );
 }
