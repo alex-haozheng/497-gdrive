b2361962baca2f0ccf8097b3e82ac58640f80f05
kayslaouar <1kayslaouar@gmail.com>
2022-12-07 03:18:17 -0500

    various updates

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -2,42 +2,20 @@ import * as express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 const app = express();
+import isAdmin from './auth.js';
+import { Analytics, File } from './interfaces.js';
 
 app.use(express.json());
 app.use(cors());
 
-function isAuth(req, res, next) {
-  if (req.isAuthenticated()) {
-      next(); // calls next middleware
-  } else {
-      res.status(401).send('Unauthorized access');
-  }
-}
+let files: File[];
+let badfiles: File[];
 
-function isAdmin (req, res, next) {
-  if (req.isAuthenticated() && req.user.admin) {
-      next();
-  } else {
-      res.status(401).send('Unauthorized admin access');
-  }
-}
-
-interface Analytics {
-	numFiles: number;
-	readabilityDistribution: {
-		[key: string | number]: number;
-	},
-  badfiles: string[];
-}
-
-interface File {
-  fileId: string,
-  content: string
-}
-
-let data: Analytics;
-let files = [];
-let badfiles;
+let analytics: Analytics = {
+  numFiles: files.length,
+  readabilityDistribution: {},
+  badfiles: badfiles
+};
 
 // for Justin to add to event-bus
 /* app.post('/events', (req, res) => {
@@ -46,64 +24,61 @@ let badfiles;
 	}
 }); */
 
-setInterval(() => {
-	Promise.all(
-    [axios.post('http://event-bus:4012/events', {
-		type: 'ShootFileAnalytics'
-	}), axios.post('http://event-bus:4012/events', {
-		type: 'ShootWordAnalytics'
-	})]);
-
-  const analytics: Analytics = {
-    numFiles: Object.entries(files).length,
-    readabilityDistribution: {},
-    badfiles: badfiles.slice()
-  }
-
-  // uses Coleman-Liau index
-  // L = avg number of letters per 100 words
-  // W = avg number of sentences per 100 words
-  // index = 0.0588 * L - 0.296 * S - 15.8;
+function processFiles(): number[] {
   const indexes: number[] = [];
-
   for (const file of files) {
-    const sentences: string[] = file.content.split("[\\p{Punct}\\s]+");
-    const words: string[] = file.content.split(/[^a-zA-Z\d]/);
-    const letters: string[] = words.join('').split('');
-    const L: number = letters.length / words.length * 100;
-    const S: number = sentences.length / letters.length * 100;
-    const index: number = 0.0588 * L - 0.296 * S - 15.8;
-    indexes.push(Math.floor(index));
+		const sentences: string[] = file.content.split('[\\p{Punct}\\s]+');
+		const words: string[] = file.content.split(/[^a-zA-Z\d]/);
+		const letters: string[] = words.join('').split('');
+		const L: number = (letters.length / words.length) * 100;
+		const S: number = (sentences.length / letters.length) * 100;
+    // uses Coleman-Liau index https://en.wikipedia.org/wiki/Coleman–Liau_index
+	  // L = avg number of letters per 100 words
+	  // W = avg number of sentences per 100 words
+	  // index = 0.0588 * L - 0.296 * S - 15.8;
+		const index: number = 0.0588 * L - 0.296 * S - 15.8;
+		indexes.push(Math.floor(index));
+	}
+  return indexes.map(n => Math.floor(n)).sort((a, b) => (a <= b ? -1 : 1));
+}
+
+function condense(data: number[]): { [key: string | number]: number } {
+  let i: number = 0;
+  const distribution: { [key: string | number]: number } = {};
+  while (i < data.length) {
+    let j: number = i;
+    while (data[++i] === data[j]);
+    distribution[data[j]] = i - j;
   }
+  return distribution;
+} // condense same data to one point and count. Ex: 1 1 1 1 2 2 2 => { 1: 4, 2: 3 }
 
-  indexes.sort((a, b) => a <= b ? -1 : 1);
+setInterval(() => {
+	Promise.all([
+		axios.post('http://event-bus:4012/events', {
+			type: 'ShootFileAnalytics'
+		}),
+		axios.post('http://event-bus:4012/events', {
+			type: 'ShootWordAnalytics'
+		})
+	]);
 
-  function condense(data: number[]) {
-    let i: number = 0;
-    const distribution: { [key: string | number]: number } = {};
-    while (i < data.length) {
-      let j: number = i;
-      while (data[++i] === data[j]);
-      distribution[data[j]] = i - j;
-    }
-    return distribution;
-  } // condense same data to one point and count. Ex: 1 1 1 1 2 2 2 => { 1: 4, 2: 3 }
-
-  analytics.readabilityDistribution = condense(indexes);
-  data = analytics;
+	const indexes: number[] = processFiles();
+  analytics.numFiles = files.length;
+	analytics.readabilityDistribution = condense(indexes);
+  analytics.badfiles = badfiles;
 }, 1000 * 60 * 60 * 24); // run once a day
 
 app.post('/events', (req, res) => {
-  if (req.body.type === 'GetWordAnalytics') {
-    badfiles = req.body.data.badfiles;
-  } else if (req.body.type === 'GetFileAnalytics') {
-    files = req.body.data.files
-  }
-
+	if (req.body.type === 'GetWordAnalytics') {
+		badfiles = req.body.data.badfiles;
+	} else if (req.body.type === 'GetFileAnalytics') {
+		files = req.body.data.files;
+	}
 });
 
 app.get('/analytics', isAdmin, (req, res) => {
-  res.send(data);
+	res.send(analytics);
 });
 
 app.listen(4004, () => {
