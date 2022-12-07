12e689a296729fb805e83a4fe3bb457de11de6d0
kayslaouar <1kayslaouar@gmail.com>
2022-12-07 02:45:40 -0500

    various updates

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -1,22 +1,33 @@
 import * as express from 'express';
-import logger from 'morgan';
-import { randomBytes } from 'crypto';
 import cors from 'cors';
 import axios from 'axios';
-import { isAdmin } from './auth.js';
-
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
+
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+      next(); // calls next middleware
+  } else {
+      res.status(401).send('Unauthorized access');
+  }
+}
+
+function isAdmin (req, res, next) {
+  if (req.isAuthenticated() && req.user.admin) {
+      next();
+  } else {
+      res.status(401).send('Unauthorized admin access');
+  }
+}
 
 interface Analytics {
 	numFiles: number;
 	readabilityDistribution: {
 		[key: string | number]: number;
 	},
-  badwords: string[];
+  badfiles: string[];
 }
 
 interface File {
@@ -25,29 +36,28 @@ interface File {
 }
 
 let data: Analytics;
+let files = [];
+let badfiles;
 
 // for Justin to add to event-bus
-/* app.post('events', (req, res) => {
+/* app.post('/events', (req, res) => {
 	if (req.body.type === 'Analytics') {
 		res.send(files);
 	}
 }); */
 
-setInterval(async () => {
-	const files: File[] = (await axios.post('http://event-bus:4012/events', {
-		type: 'FileAnalytics',
-    data: {}
-	})).data.files;
-
-  const badwords: string[] = (await axios.post('http://event-bus:4012/events', {
-		type: 'WordAnalytics',
-    data: {}
-	})).data.badwords;
+setInterval(() => {
+	Promise.all(
+    [axios.post('http://event-bus:4012/events', {
+		type: 'ShootFileAnalytics'
+	}), axios.post('http://event-bus:4012/events', {
+		type: 'ShootWordAnalytics'
+	})]);
 
   const analytics: Analytics = {
     numFiles: Object.entries(files).length,
     readabilityDistribution: {},
-    badwords: badwords.slice()
+    badfiles: badfiles.slice()
   }
 
   // uses Coleman-Liau index
@@ -55,7 +65,7 @@ setInterval(async () => {
   // W = avg number of sentences per 100 words
   // index = 0.0588 * L - 0.296 * S - 15.8;
   const indexes: number[] = [];
-  
+
   for (const file of files) {
     const sentences: string[] = file.content.split("[\\p{Punct}\\s]+");
     const words: string[] = file.content.split(/[^a-zA-Z\d]/);
@@ -68,11 +78,11 @@ setInterval(async () => {
 
   indexes.sort((a, b) => a <= b ? -1 : 1);
 
-  function condense(data) {
-    let i = 0;
-    const distribution = {};
+  function condense(data: number[]) {
+    let i: number = 0;
+    const distribution: { [key: string | number]: number } = {};
     while (i < data.length) {
-      let j = i;
+      let j: number = i;
       while (data[++i] === data[j]);
       distribution[data[j]] = i - j;
     }
@@ -83,7 +93,18 @@ setInterval(async () => {
   data = analytics;
 }, 1000 * 60 * 60 * 24); // run once a day
 
-app.get('/analytics', isAdmin, (req, res) => {});
+app.post('/events', (req, res) => {
+  if (req.body.type === 'GetWordAnalytics') {
+    badfiles = req.body.data.badfiles;
+  } else if (req.body.type === 'GetFileAnalytics') {
+    files = req.body.data.files
+  }
+
+});
+
+app.get('/analytics', isAdmin, (req, res) => {
+  res.send(data);
+});
 
 app.listen(4004, () => {
 	console.log('Listening on 4004');
