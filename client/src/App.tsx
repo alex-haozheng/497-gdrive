e602a05cc7c7caef8a5ce1e1b8ec8952b13b5dc4
Yuri Kim <yuri7kim@gmail.com>
2022-12-09 02:30:03 -0500

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import Login from './Login.js';
 import AdminList from './AdminList.js';
 
 function App() {
   return (
     <div className="App">
+      <h1>Login</h1>
       <Login />
+      <h1>Admin</h1>
       <AdminList />
     </div>
   );
