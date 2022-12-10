08fbaa82dd7ab3eff66b136c413fdf1bfa8c997c
sazzle2 <azheng@umass.edu>
2022-12-10 14:14:08 -0500

    changed file name

---
+++ questions/README.md
@@ -0,0 +1,72 @@
+# Author: 
+Alex Zheng
+
+# Github: 
+sazzle2
+
+# Service Description: 
+This service will handle the event in which a user loses their account information via password, the plan is to allow users to use a one time password then allow them to change their old password, basically trying to imitate the process online when a user forgets their password. Users will be brought to this page after registration to store the security questions before the complete access
+
+# Interaction with other services: 
+will send events to the users endpoint to change the change password flag, will also be receiving user created and deleted events to update the database on our end to know whether this user exists or not to be asked this code
+
+# Endpoint Information: 
+
+`GET /login/:email/forgotpw`
+
+Description: This endpoint will send an email out to the recipients with the following account information
+
+- will add endpoints as I go
+
+# How to run service:
+
+### **Step 1: Prerequisites**
+
+- [Node](https://nodejs.org/en/)
+- [NPM](https://www.npmjs.com/)
+- [VSCode](https://code.visualstudio.com/)
+    - Install the appropriate language support for each language used in the project.
+- [React.js](https://reactjs.org/)
+- [Git](https://git-scm.com/)
+- [Express.js](https://expressjs.com/)
+- [MongoDB](https://www.mongodb.com/)
+- [Docker](https://www.docker.com/)
+- [Kubernetes](https://kubernetes.io/)
+
+### **Step 2: Clone the Repository**
+
+- Navigate to the desired project directory on your computer.
+
+- Clone the repository from [GitHub](https://github.com/umass-cs-497s-F22/milestone-2-implementation-team0.git) using the `git clone` command.
+
+    ```bash
+    $ git clone https://github.com/umass-cs-497s-F22/milestone-2-implementation-team0.git
+    ```
+
+- Navigate to the cloned repository directory.
+
+    ```bash
+    $ cd name-of-cloned-repository
+    ```
+### **Step 3: Install Dependencies**
+
+- Check that the terminal is in the correct directory.
+
+    ```bash
+    $ pwd
+    ```
+
+- Install the dependencies using the `npm install` command.
+
+    ```bash
+    $ npm install
+    ```
+### **Step 4: Run the Application**
+
+- Run the application using the `npm start` command.
+
+    ```bash
+    $ npm start
+    ```
+### **Step 5: View the Application**
+- The command from Step 4 will locally host the website on `http://localhost:3000`.
---
+++ questions/index.ts
@@ -0,0 +1,104 @@
+import * as express from 'express';
+import { Request, Response } from 'express';
+import * as logger from 'morgan';
+import * as cors from 'cors';
+//import { nodemailer } from 'nodemailer';
+const nodemailer = require('nodemailer');
+import { faker } from '@faker-js/faker';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+
+interface database {
+	[key: string]: string
+};
+
+//holds a collection of all emails that are registered
+// store uid along with security question
+const db: database = {};
+
+// uid: team0.clouddrive@gmail.com
+// password: ourpassword
+
+// return the verified emails (used for account creation with existing email)
+app.get('/emails', (req: Request, res: Response) => {
+	res.send(Object.keys(db));
+});
+
+app.get('/login/forgotpw', async (req: Request, res: Response) => {
+	try {
+		const { uid, email }: { uid: string, email: string } = req.body;
+		const otp: string = faker.internet.password();
+		
+		if (!(uid in db)) {
+			res.status(400).json({
+				message: 'NOT FOUND'
+			}); return;
+		}
+
+		// right around here add a await call to another endpoint to change the password and mark a flag
+		// await axios.post('http://event-bus:4012/events', {
+		// 	type: "ChangePassword",
+		// 	data: {
+		// 		uid, // username
+		// 		otp
+		// 	}
+		// });
+		// let's create the transport (it's the postman/delivery-man who will send your emails)
+		const myTransport = nodemailer.createTransport({
+			service: 'Gmail',
+			auth: {
+				user: 'team0cloud@hotmail.com', // your gmail account which you'll use to send the emails
+				pass: 'ourpassword!', // the password for your gmail account
+			}
+		});
+		// defining the content of the email (I mean, what will be on the email)
+		const mailOptions = {
+			from: 'team0cloud@hotmail.com', // from where the email is going, you can type anything or any name here, it'll be displayed as the sender to the person who receives it
+			to: email, // the email address(es) where you want to send the emails to. If it's more than one person/email, seperate them with a comma, like here how I seperated the 3 users with a comma
+			
+			subject: 'Sending Some Freaking Email', // your email subject (optional but better to have it)
+			text: `Hello there my sweetling! Let's send some freaking emails!\n Here is your one time password: ${otp}` // your email body in plain text format (optional) 
+			
+			// your email body in html format (optional)
+			// if you want to send a customly and amazingly designed html body
+			// instead of a boring plain text, then use this "html" property
+			// instead of "text" property
+			// html: `<h1 style="color: red;text-align:center">Hello there my sweetling!</h1>
+			// 			<p style="text-align:center">Let's send some <span style="color: red">freaking</span> emails!</p>`,
+		}
+		
+		// sending the email
+		myTransport.sendMail(mailOptions, (err) => {
+			if (err) {
+				console.log(`Email failed to send!`);
+				console.error(err);
+			} else {
+				console.log(`Email successfully sent!`);
+			}
+		})
+		// should probably change this output later (not necessary)
+		res.status(200).json(email);
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
+app.post('/events', (req: Request, res: Response) => {
+	const {type, data }: {type: string, data: { uid: string, email?: string }} = req.body;
+	if (type === 'AccountCreated') {
+		const { uid , email }: { uid: string, email?: string } = data;
+		db[uid] = email!;
+	} else if (type === 'AccountDeleted') {
+		const { uid }: { uid: string, email?: string } = data;
+		delete db[uid];
+	}
+	res.send({status: 'ok'});
+});
+
+app.listen(4006, () => {
+	console.log('Listening on 4006');
+});
