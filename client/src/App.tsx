c6aaf07efb6835f735dba6aac0bd532c02ffe9ae
sazzle2 <azheng@umass.edu>
2022-12-11 23:55:13 -0500

    app updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,10 +31,10 @@ function App() {
     <div className="App">
       <Login func={getUIDandToken} />
       <Register />
+      <Questions uid = {uid}/>
+      <ForgotQuestions uid = {uid}/>
       <Profile uid={"user0"}/>
       <Admin uid={"user5"}/>
-      <Questions uid = {'test'}/>
-      <ForgotQuestions uid = {'test'}/>
 
       <h1>Files</h1>
       {/* uncomment the line below to see a working concept of the landing page where all the files are shown on. Only works when fileService and uploadDownload service are running.} */}
