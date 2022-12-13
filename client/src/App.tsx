1afa18e215c127f4eedd1a408008401c0df2ee5d
kayslaouar <1kayslaouar@gmail.com>
2022-12-12 19:04:09 -0500

    fixed route proprs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -57,9 +57,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         {/* protected routes, will only be rendered if the user is logged in */}
         <Route path="/questions" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<Questions uid = {localStorage.getItem('uid')} accessToken = {localStorage.getItem('accessToken')} />} </ProtectedRoute>} />
-        <Route path="/forgotquestions" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<ForgotQuestions uid = {localStorage.getItem('uid')} accessToken = {localStorage.getItem('accessToken')} />} </ProtectedRoute>} />
-        <Route path="/profile" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<Profile uid = {localStorage.getItem('uid')} accessToken = {localStorage.getItem('accessToken')} />} </ProtectedRoute>} />
-        <Route path="/admin" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<Admin uid = {localStorage.getItem('uid')} accessToken = {localStorage.getItem('accessToken')} />} </ProtectedRoute>} />
+        <Route path="/forgotquestions" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<ForgotQuestions  />} </ProtectedRoute>} />
+        <Route path="/profile" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<Profile uid = {localStorage.getItem('uid')} />} </ProtectedRoute>} />
+        <Route path="/admin" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<Admin uid = {localStorage.getItem('uid')} />} </ProtectedRoute>} />
         <Route path="/analytics" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<Analytics uid = {localStorage.getItem('uid')} accessToken = {localStorage.getItem('accessToken')} />} </ProtectedRoute>} />
         <Route path="/files/*" element = {<ProtectedRoute uid={localStorage.getItem('uid')} accessToken={localStorage.getItem('accessToken')}> {<LandingPage />} </ProtectedRoute>} />
       </Routes>
